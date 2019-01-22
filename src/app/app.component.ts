18944f92eb2dbb2734a3bf8a52e489eb11a899ba
gianni <gianniresende@gmail.com>
2019-01-22 21:31:49 -0200

    add bootstrap and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Gerenciador de Tarefas!';
+  task = 'Enviar orçamento para o cliente X'
 }
