6ac7ee55104f19f21079b9f0ba4dae8a6aad8363
gianni <gianniresende@gmail.com>
2019-01-23 21:57:51 -0200

    binding from view to logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { TasksComponent } from "./tasks/tasks.component";
+import { LearningBindingsComponent } from './learning-bindings/learning-bindings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    TasksComponent
+    TasksComponent,
+    LearningBindingsComponent
   ],
   imports: [
     BrowserModule,
