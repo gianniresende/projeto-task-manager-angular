4e3266266cebca4c68f5dd9570db059c8731e687
gianni <gianniresende@gmail.com>
2019-01-27 16:14:17 -0200

    Adding dashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { LearningBindingsComponent } from "./learning-bindings/learning-bindings.component";
 import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
 
+const ROUTES = RouterModule.forRoot([
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+])
+
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     NavbarComponent,
     TasksComponent,
     LearningBindingsComponent,
@@ -20,7 +39,8 @@ import { TaskDetailComponent } from "./tasks/task-detail/task-detail.component";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,    
+    ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
